ae0a45937eb28926fe0a94f91605699cb2bd4fac
Chris <cjaneczek@student.tgm.ac.at>
2014-10-31 14:13:14 +0100

    changes in SSHReader and SSHProperties

--- Project/RockTheNet/src/engine/network/SSHProperties.java
+++ Project/RockTheNet/src/engine/network/SSHProperties.java
@@ -4,8 +4,37 @@ import java.util.Properties;
 
 /**
  * Equivalent to OIDProperties, stores the information required for the SSH-Readers and -Writers
- * @version 2014-10-23
+ * @version 2014-10-31
+ * @author Christian Janeczek
  */
 public class SSHProperties extends Properties{
+	
+	//The properties of SSH are currently stored statically.
+	//TODO One of the GUI-elements should refer to an instance of SSHProperties and
+	//TODO deliver the values dynamically (getter & setter)
+	
+	private static final long serialVersionUID = -2393053076040065937L;
+	private String hostname = "10.0.100.10";
+	private String username = "5ahit";
+	private String password = "Waeng7ohch8o";
+	
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 }
--- Project/RockTheNet/src/engine/network/SSHReader.java
+++ Project/RockTheNet/src/engine/network/SSHReader.java
@@ -1,21 +1,43 @@
 package engine.network;
 
+import java.io.IOException;
+import application.RTNMain;
 import java.util.List;
+import org.apache.log4j.Logger;
 
 /**
- * This class is used to read with SSH, not required for now (because SNMP works as well)
- * @version 0.1
+ * This class is used to read with SSH
+ * The firewall needs some sort of CRUD
+ * @version 2014-10-31
+ * @author Christian Janeczek
  */
 public class SSHReader implements PolicyReader {
 	
-	private SSHProperties sSHProperties;
-
-
-	/**
-	 * @see engine.network.PolicyReader#read()
-	 */
-	public List<List<String>> read() {
-		return null;
+	private static final Logger logger = Logger.getLogger(RTNMain.class);
+	private SSHProperties sshProperties;
+	private String hostname, username, password;
+	
+	public SSHReader() throws IOException{
+		
+		//initializing the values, which are needed for reading the policies
+		this.sshProperties = new SSHProperties();
+		this.hostname = this.sshProperties.getHostname();
+		this.username = this.sshProperties.getUsername();
+		this.password = this.sshProperties.getPassword();
+		
+		//I tried the following libraries:
+		//*Jsch
+		//*sshj
+		//etc.
+		//They had no documentation at all
+		
 	}
 
+	@Override
+	public List<List<String>> read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
+
+
